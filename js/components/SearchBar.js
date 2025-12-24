b096d67cecd90b42a10a5a228060dbea54286e1c
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2025-12-24 08:34:34 +0000

    feat(ui): add SearchBar component
    Add a new SearchBar component that renders a search input with placeholder text and icons for search and settings. This component is part of the UI feature for searching notes by title, content, or tags.

---
+++ js/components/SearchBar.js
@@ -0,0 +1,17 @@
+export const SearchBar = () => `
+<section class="search-bar">
+<p>All Note</p>
+<section class="section-settings">
+  <label for="search-input">
+    <img src=".././assets/images/icon-search.svg"/>
+    <input
+      id="search-input"
+      class="search-input"
+      placeholder="Search by title, content, or tags..."
+    />
+  </label>
+ </label>
+  <img src=".././assets/images/icon-settings.svg"/>
+</section>
+</section>
+`;
