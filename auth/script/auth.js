cf10d35ce7b93afb17d387ab6848d02159376752
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2025-12-27 19:11:43 +0000

    feat: introduce forget password files

--- auth/script/auth.js
+++ auth/script/auth.js
@@ -5,7 +5,7 @@ const toggleIcon = toggleBtn.querySelector("img");
 toggleBtn.addEventListener("click", () => {
   const isPassword = passwordInput.type === "password";
   passwordInput.type = isPassword ? "text" : "password";
-  toggleIcon.src = isPassword 
+  toggleIcon.src = isPassword
     ? "../assets/images/icon-hide-password.svg"
     : "../assets/images/icon-show-password.svg";
 });
